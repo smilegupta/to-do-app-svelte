99a5690777d8ee3abc0f9b805febfa81528b60cb
smilegupta <guptamiley3012@gmail.com>
2021-09-21 15:58:47 +0530

    Added more test case

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,11 +1,15 @@
 import "@testing-library/jest-dom";
+import { render } from "@testing-library/svelte";
 
-import { fireEvent, render } from "@testing-library/svelte";
-
+import App from "./App.svelte";
 import Header from "./components/Header.svelte";
+
+test("should render", () => {
+  const results = render(App);
+    expect(results).toBeDefined();
+});
 
 test("shows proper heading when rendered", () => {
   const { getByText } = render(Header, { name: "Smile" });
-
   expect(getByText(`Smile's To-Do List`)).toBeInTheDocument();
 });
